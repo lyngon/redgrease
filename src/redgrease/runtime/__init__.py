60ef2a1c8b8e192bab3459404a0765401c081a3f
d00astro <anders@lyngon.com>
2020-11-26 18:09:34 +0800

    Cleanup of runtime __init__

--- src/redgrease/runtime/__init__.py
+++ src/redgrease/runtime/__init__.py
@@ -1,6 +1,16 @@
 import sys
 
-if 'redisgears' not in sys.modules:
+if 'redisgears' in sys.modules:
+    # Import the default functions and classes
+    from redisgears import atomicCtx as atomic  # noqa: F401
+    from redisgears import executeCommand as execute  # noqa: F401
+    from redisgears import getMyHashTag as hashtag  # noqa: F401
+    from redisgears import log  # noqa: F401
+    from __main__ import gearsConfigGet  # noqa: F401
+    from __main__ import GearsBuilder  # noqa: F401
+    from __main__ import GB  # noqa: F401
+else:
+    # Import placeholder functions and classes
     from .placeholders import (   # noqa: F401
         atomic,
         execute,
@@ -11,20 +21,3 @@ if 'redisgears' not in sys.modules:
         GearsBuilder,
         GB,
     )
-else:
-    from redisgears import atomicCtx as atomic  # noqa: F401
-    from redisgears import executeCommand as execute  # noqa: F401
-    from redisgears import getMyHashTag as hashtag  # noqa: F401
-    from redisgears import log  # noqa: F401
-    from __main__ import gearsConfigGet  # noqa: F401
-    from __main__ import GearsBuilder  # noqa: F401
-    from __main__ import GB  # noqa: F401
-
-    # atomic = atomic
-    # execute = execute
-    # hashtag = hashtag
-    # configGet = configGet
-    # gearsConfigGet = gearsConfigGet
-    # log = log
-    # GearsBuilder = GearsBuilder
-    # GB = GB
