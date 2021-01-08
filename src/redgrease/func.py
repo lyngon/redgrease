852a165b26afce7dbed81b6bf81069edab974b3c
Anders Astrom <anders@lyngon.com>
2021-01-08 17:23:23 +0800

    simple trigger function decorator

---
+++ src/redgrease/func.py
@@ -0,0 +1,28 @@
+from redgrease.runtime import GearsBuilder
+from redgrease import TriggerMode
+from redgrease.typing import Callback
+
+
+def trigger(
+    trigger: str,
+    prefix: str = '*',
+    convertToStr: bool = True,
+    collect: bool = True,
+    mode: str = TriggerMode.Async,
+    onRegistered: Callback = None,
+    **kargs
+):
+    def gear(func):
+        GearsBuilder("CommandReader") \
+            .map(lambda params: func(params[1:])) \
+            .register(
+                prefix=prefix,
+                convertToStr=convertToStr,
+                collect=collect,
+                mode=mode,
+                onRegistered=onRegistered,
+                trigger=trigger,
+                **kargs
+            )
+    return gear
+
