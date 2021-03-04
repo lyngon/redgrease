6c4b706b745e320edc591937ad13e20893c70844
d00astro <anders@lyngon.com>
2021-03-04 08:53:26 +0800

    re.Pattern python 3.6 compatibility

--- src/redgrease/loader.py
+++ src/redgrease/loader.py
@@ -20,6 +20,11 @@ default_requirements_pattern = "*requirements*.txt"
 default_unblocking_pattern = "unblock"
 default_ignore_patterns: List[str] = []
 
+# Regex type changed
+# from re.SRE_Pattern in Python 3.6
+# to re.Pattern in Python3 3.7
+regex_type = type(re.compile(""))
+
 
 def fail(exception, *messages):
     """Convenience function for raising exceptions
@@ -68,7 +73,7 @@ class GearsLoader:
             unblocking_pattern = default_unblocking_pattern
         if isinstance(unblocking_pattern, str):
             unblocking_pattern = re.compile(re.escape(unblocking_pattern))
-        if not isinstance(unblocking_pattern, re.Pattern):
+        if not isinstance(unblocking_pattern, regex_type):
             fail(ValueError, f"Invalid unblocking pattern: {unblocking_pattern}")
         self.unblocking_pattern = unblocking_pattern
 
