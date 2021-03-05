5c3be5fa659fbcebccbd4035da22c2201042eac4
d00astro <anders@lyngon.com>
2021-03-05 13:08:10 +0800

    Copyright, Licence and author blurb, and module docstring placeholder

--- publish.py
+++ publish.py
@@ -1,4 +1,49 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Builds the package and publises it to PyPi
+Increasess the pacth number by one, in the 'setup.cfg' file, unless otherwise stated,
+ before building.
+
+Args:
+    --current-version: Only reads and returns the current version from 'setup.cfg'
+    --read-version: Same as '--current-version'
+    rc: Release as a release candidate version (eg 1.2.3.rc1)
+    dev: Release as a development version (eg 1.2.3.dev1)
+    test: Same as 'dev'
+    major: Increase major version by one and set minor and patch to zero.
+        e.g: 1.2.3 becomes 2.0.0
+    minor: Increase minor version by one and set patch to zero.
+        e.g: 1.2.3 becomes 1.3.0
+    -y: Auto-confirm, i.e. Do not require manual confirmation of version
+
+Todo:
+    * Simplify, not modify the version in 'setup.cfg' but just use as input
+
+"""
+__author__ = "Anders Åström"
+__contact__ = "anders@lyngon.com"
+__copyright__ = "2021, Lyngon Pte. Ltd."
+__licence__ = """The MIT License
+Copyright © 2021 Lyngon Pte. Ltd.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ software and associated documentation files (the “Software”), to deal in the Software
+ without restriction, including without limitation the rights to use, copy, modify,
+ merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to the following
+ conditions:
+
+The above copyright notice and this permission notice shall be included in all copies
+ or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 import configparser
 import subprocess
