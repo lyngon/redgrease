fd6c284f14e5ede60d3b3464cfa34c75fe3af074
d00astro <anders@lyngon.com>
2021-03-22 15:39:50 +0800

    fixed cluster commands

--- src/redgrease/cluster.py
+++ src/redgrease/cluster.py
@@ -2,6 +2,12 @@ import rediscluster
 import rediscluster.exceptions
 
 import redgrease.client
+import redgrease.data
+import redgrease.utils
+
+
+def nop(*args):
+    return args
 
 
 @redgrease.client.geared
@@ -15,6 +21,37 @@ class RedisCluster(rediscluster.RedisCluster):
         gears (redgrease.client.Gears):
             Gears command client.
     """
+
+    # States how target node is selected for cluster commands:
+    # - blocked : command is not allowed - Raises a RedisClusterException
+    # - random : excuted on one randomly selected node
+    # - all-masters : executed on all master node
+    # - all-nodes : executed on all nodes
+    # - slot-id : executed on the node defined by the second argument
+    NODES_FLAGS = {
+        **rediscluster.RedisCluster.NODES_FLAGS,
+        **{
+            "RG.INFOCLUSTER": "random",
+            "RG.PYSTATS": "all-nodes",
+            "RG.PYDUMPREQS": "random",
+            "RG.REFRESHCLUSTER": "all-nodes",
+        },
+    }
+
+    # Not to be confused with redis.Redis.RESPONSE_CALLBACKS
+    # RESULT_CALLBACKS is special to rediscluster.RedisCluster.
+    # It decides how results of commands defined in `NODES_FLAGS` are aggregated into
+    # a final response, **after** redis.Redis.RESPONSE_CALLBACKS as been applied to
+    # each response individually.
+    RESULT_CALLBACKS = {
+        **rediscluster.RedisCluster.RESULT_CALLBACKS,
+        **{
+            "RG.INFOCLUSTER": lambda _, res: next(iter(res.values())),
+            "RG.PYSTATS": lambda _, res: res,
+            "RG.PYDUMPREQS": lambda _, res: next(iter(res.values())),
+            "RG.REFRESHCLUSTER": lambda _, res: all(res.values()),
+        },
+    }
 
     def __init__(self, *args, **kwargs):
         """Instantiate a redis cluster client, with gears features"""
